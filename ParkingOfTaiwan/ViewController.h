7068b2ff14c7f58fa35ec8eb739bb2bb18e5deb4
LazyScream <screamlazy@lazyscreamdemacbook-pro.local>
2016-05-26 00:16:51 +0800

    Initial Commit

---
+++ ParkingOfTaiwan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParkingOfTaiwan
+//
+//  Created by LazyScream on 2016/5/26.
+//  Copyright © 2016年 LazyScream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParkingOfTaiwan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ParkingOfTaiwan
+//
+//  Created by LazyScream on 2016/5/26.
+//  Copyright © 2016年 LazyScream. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
